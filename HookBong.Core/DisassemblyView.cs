1bb14f43568b27ccd84646c8794f773b5d7be300
Brit <Admin>
2020-07-27 22:00:24 +0200

    string makers
    waaa

--- HookBong.Core/DisassemblyView.cs
+++ HookBong.Core/DisassemblyView.cs
@@ -14,7 +14,7 @@ namespace HookBong.Core
         }
 
 
-        public static string[] DisassembleToLines(Byte[] codeBytes, ulong RIP)
+        public static List<string> DisassembleToLines(Byte[] codeBytes, ulong RIP)
         {
             var disassOut = new List<string>();
             
@@ -26,11 +26,9 @@ namespace HookBong.Core
             var formatter = new MasmFormatter();
             var output = new FormatterOutputImpl();
 
-            var outText = "";
-
             foreach (var instr in decoder) {
                 output.List.Clear();
-                outText = "";
+                var outText = "";
                 formatter.Format(instr, output);
                 foreach (var (text, kind) in output.List) {
                     outText += (text);
@@ -38,7 +36,50 @@ namespace HookBong.Core
                 disassOut.Add(outText);
             }
 
-            return disassOut.ToArray();
+            return disassOut;
         }
+
+        public static List<string> FormatBytes(Byte[] codeBytes, ulong RIP)
+        {
+            var decodeOut = new List<string>();
+            
+            var codeReader = new ByteArrayCodeReader(codeBytes);
+            var decoder = Decoder.Create(Environment.Is64BitProcess? 64 : 32, codeReader);
+            decoder.IP = RIP;
+            var endRip = decoder.IP + (uint)codeBytes.Length;
+            
+            var instructions = new InstructionList();
+            while (decoder.IP < endRip) {
+                decoder.Decode(out instructions.AllocUninitializedElement());
+            }
+
+            var formatter = new NasmFormatter();
+
+            formatter.Options.DigitSeparator = "`";
+            formatter.Options.FirstOperandCharIndex = 10;
+
+            var output = new StringOutput();
+
+            foreach (ref var instr in instructions) {
+                formatter.Format(instr, output);
+                var outText = (instr.IP.ToString("X16"));
+                outText += (" ");
+                var instrLen = instr.Length;
+                var byteBaseIndex = (int)(instr.IP - RIP);
+                for (var i = 0; i < instrLen; i++)
+                    outText += (codeBytes[byteBaseIndex + i].ToString("X2"));
+                var missingBytes = 10 - instrLen;
+                for (var i = 0; i < missingBytes; i++)
+                    outText +=("  ");
+                outText +=(" ");
+                outText += (output.ToStringAndReset());
+                decodeOut.Add(outText);
+            }
+
+            return decodeOut;
+        }
+
+
+
     }
 }
